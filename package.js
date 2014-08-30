43afd93f26f100f19ba48d8398ab1dec82b8769b
Sebastian Clausen <sebastian.clausen@bilfinger.com>
2014-08-30 15:22:36 +0200

    v0.1.1

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
   summary: 'Displaying any number as a tally',
-  version: '0.1.0',
+  version: '0.1.1',
   git: "https://github.com/sclausen/meteor-tally.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
+  api.use('templating', 'client');
   api.addFiles('lib/tally.html', 'client');
   api.addFiles('lib/tally.js', 'client');
   api.addFiles('lib/tally-mark.regular.ttf', 'client');
